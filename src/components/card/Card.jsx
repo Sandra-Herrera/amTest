e13fa13ea171075573768476df5ef400d138a11e
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-07-27 13:15:13 -0500

    Cards styles

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,38 +2,58 @@ import React from "react";
 import styles from "./card.module.scss";
 import iconFavorites from "../../img/iconFavorites.png";
 
-const Card = () => {
+
+const Card = (props) => {
+  const character = props.character;
+  const getColor = (house) => {
+    if(house.toLowerCase() === "slytherin"){
+      return styles.slytherinGrandient
+    }
+    else if(house.toLowerCase() === "ravenclaw"){
+      return styles.ravenclawGrandient
+    }
+    else if(house.toLowerCase() === "hufflepuff"){
+      return styles.hufflepuffGrandient
+    }
+    else if(house.toLowerCase() === "gryffindor"){
+      return styles.gryffindorGrandient
+    }
+    else{
+      return styles.whiteBackgroundColor
+    }
+  }
+
   return (
     <>
-      <section className={styles.cardCharacter}>
-        <section className={styles.photoContainer}>
-          <img alt="photoStudent"></img>
-        </section>
-
+    <section className={styles.cardCharacter}>
+          <section className={`${styles.photoContainer} ${getColor(character.house)}`}>
+            <figure className={styles.image} style={{backgroundImage: 'url(' + character.image + ')'}}></figure>
+          </section>
+        
         <section className={styles.infoCharacter}>
-          <h2 className={styles.name}>Harry Potter</h2>
+          <h2 className={styles.name}>{character.name}</h2>
           <section className={styles.topSectionCard}>
-            <p className={styles.status}>VIVO / ESTUDIANTE</p>
-            <button className={styles.buttonSave}>
+            <span className={styles.status}>{character.alive ? 'VIVO':'MUERTO'} / {character.hogwartsStaff ? 'STAFF' : (character.hogwartsStudent ? 'ESTUDIANTE' : 'OTROS')}</span>
+            <button className={styles.buttonFav}>
               <img alt="icon" src={iconFavorites}></img>
             </button>
           </section>
-          <section>
+          <section className={styles.bottomCard}>
             <section>
               <span>Cumpleaños:</span>
-              <span>31-07-1980</span>
+              <span>{character.dateOfBirth}</span>
             </section>
             <section>
               <span>Genero:</span>
-              <span>Male</span>
+              <span>{character.gender}</span>
             </section>
             <section>
               <span>Color de ojos:</span>
-              <span>Green</span>
+              <span>{character.eyeColour}</span>
             </section>
             <section>
               <span>Color de pelo:</span>
-              <span>Black</span>
+              <span>{character.hairColour}</span>
             </section>
           </section>
         </section>
