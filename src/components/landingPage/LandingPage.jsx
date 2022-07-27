aa8158e167bce64e4b58622a1356a0c1d30c5a55
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-07-27 13:17:37 -0500

    render cards in landing page

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -4,21 +4,33 @@ import Header from "../header/Header";
 import Card from "../card/Card";
 import styles from "./landingPage.module.scss";
 import Footer from "../footer/Footer";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getCharacters } from "../../store/slices/charactersThunks"
 
 const LandingPage = () => {
+  const { characters } = useSelector((state) => state.characters);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCharacters());
+    console.log(characters);
+  }, []);
+
   return (
     <>
       <div className={styles.landingContainer}>
-        <Favorites />
+        <Favorites/>
         <Header />
         <section className={styles.cardsContainer}>
-          <Card />
-          <Card />
+          {characters.map((character, index)=>
+            <Card key={index} character={character} />
+          )}
         </section>
         <Footer />
       </div>
     </>
   );
-};
+}
 
 export default LandingPage;
